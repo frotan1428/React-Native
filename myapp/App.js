084009c6995b59de25a91eb641ae5f2ff7f41b27
Ziya Sarı <zsari.techproed@gmail.com>
2022-07-19 19:59:21 +0300

    v2

--- myapp/App.js
+++ myapp/App.js
@@ -5,26 +5,28 @@ import Flex3 from "./components/01-flex/flex3";
 import Flex4 from "./components/01-flex/flex4";
 import Flex5 from "./components/01-flex/flex5";
 import Flex6 from "./components/01-flex/flex6";
+import Flex7 from "./components/01-flex/flex7";
+import Flex8 from "./components/01-flex/flex8";
 
 const App = () => {
   return (
     <View
       style={{
-        flex:1
+        flex: 1,
       }}
     >
-{/*       
+      {/*       
 <Flex1/>
 <Flex2/>
 <Flex3/>
 <Flex4/>
 <Flex5/>
- */}    
- 
- <Flex6/>
- 
- 
- </View>
+<Flex6/>
+<Flex7 />
+ */}
+
+      <Flex8 />
+    </View>
   );
 };
 
